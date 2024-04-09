619c98fd58b674887ba122b3cc886825483a7805
Anton Ptashnik <iavtomator@gmail.com>
2024-04-09 13:49:52 +0300

    add a script for running contract upgrade tests

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# Sample Hardhat Project
+# Web3 Domain registar
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+Basic service for accounting registered domain names that runs on Ethereum.
 
-Try running some of the following tasks:
+## Testing
+
+### v1->v2 contract upgrade tests
+
+Run tests to verify post-upgrade contract integrity as below 
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.js
+npm run test_v1to2_upgrade
 ```
+
+High-level test scenario:
+1. Run all tests for contract v1 to make sure it works
+2. Deploy contract v1 into a local network
+3. Populate data for contract v1
+4. Run all tests for contract v2 to make sure it works
+5. Upgrade the deployed contract to v2
+6. Verify data populated to v1 is available for v2
+7. Verify v2 preserved v1 functionality - top level domain registration
---
+++ upgrage_check.sh
@@ -0,0 +1,29 @@
+set -em
+
+echo "Start Hardhat network"
+npx hardhat node > network.log &
+HH_NETWORK_PID=$!
+trap "kill -- -$HH_NETWORK_PID" EXIT # teardown network on finish
+
+echo "Test contract v1 separately before deploy"
+git checkout hw2p1-make-contract-upgradable
+npx hardhat test ./test/DomainRegistar.js
+
+echo "Deploy a contract v1 to a local network"
+npx hardhat run scripts/deployDomainRegistar.js --network localhost | tee out.log
+CONTRACT_ADDR=`cat out.log | sed -n "s/Contract deployed to: \([^']\+\).*/\1/p"`
+if [ -z "$CONTRACT_ADDR" ]; then echo "Could not parse contract address"; exit 1; fi
+
+echo "Test v2 contract separately before deploy"
+git checkout hw2p2-support-subdomains
+npx hardhat test ./test/DomainRegistar.js
+
+echo "Upgrade a contract in a local network"
+export CONTRACT_ADDR
+npx hardhat run scripts/upgradeDomainRegistar.js --network localhost
+
+export HARDHAT_NETWORK=localhost
+DATAFILE=test/datasets/preupgrade.json npx hardhat test --grep "support top-level domain registration"
+DATAFILE=test/datasets/preupgrade.json npx hardhat test --grep "access domains created by v1"
+
+echo "All tests pass!"
