f606e517a42c9c17688556715d5ed0953d778f11
Anton Ptashnik <iavtomator@gmail.com>
2024-04-09 15:27:54 +0300

    add a test to verify upgrade do not corrupt v1 functionality

--- upgrage_check.sh
+++ upgrage_check.sh
@@ -14,6 +14,9 @@ npx hardhat run scripts/deployDomainRegistar.js --network localhost | tee out.lo
 CONTRACT_ADDR=`cat out.log | sed -n "s/Contract deployed to: \([^']\+\).*/\1/p"`
 if [ -z "$CONTRACT_ADDR" ]; then echo "Could not parse contract address"; exit 1; fi
 
+echo "Populate data for a contract"
+DATAFILE=test/datasets/preupgrade.json npx hardhat test --grep "support top-level domain registration" --network localhost
+
 echo "Test v2 contract separately before deploy"
 git checkout hw2p2-support-subdomains
 npx hardhat test ./test/DomainRegistar.js
@@ -23,7 +26,10 @@ export CONTRACT_ADDR
 npx hardhat run scripts/upgradeDomainRegistar.js --network localhost
 
 export HARDHAT_NETWORK=localhost
-DATAFILE=test/datasets/preupgrade.json npx hardhat test --grep "support top-level domain registration"
+echo "Verify data populated to v1 accessible from v2"
 DATAFILE=test/datasets/preupgrade.json npx hardhat test --grep "access domains created by v1"
+
+echo "Verify v2 preserved top-level domain creation"
+DATAFILE=test/datasets/postupgrade.json npx hardhat test --grep "support top-level domain registration"
 
 echo "All tests pass!"
